e648bd83e5dcb070e70924c3e4ea95e8180f8b68
Adityaya Dhande <adityaya123@gmail.com>
2023-10-30 16:23:42 +0530

    agent

--- assignment3/CS747-PA3/pool/agent.py
+++ assignment3/CS747-PA3/pool/agent.py
@@ -7,7 +7,7 @@ import utils
 import time
 import config
 import numpy
-random.seed(73)
+random.seed(62)
 
 PI = math.pi
 ANGLE = numpy.linspace(-1,1,200)
@@ -27,7 +27,6 @@ class Agent:
         self.prev_balls = None
         self.ns = utils.NextState()
 
-
     def set_holes(self, holes_x, holes_y, radius):
         for x in holes_x:
             for y in holes_y:
@@ -35,50 +34,65 @@ class Agent:
         self.holes = numpy.array(self.holes)
         self.ball_radius = radius
 
-    def closest_hole(self, ballx, bally):
+    def closest_hole(self, ballx, bally, valid_holes):
         hole_dist = self.cue_dist(self.holes[:,0], self.holes[:,1], ballx, bally)
-        return numpy.argmin(hole_dist)
+        new_holes_dist = []
+        for i in range(6):
+            if i in valid_holes:
+                new_holes_dist.append(hole_dist[i])
+        return numpy.argmin(numpy.array(new_holes_dist))
 
-    def hole_angle(self, ballx, bally, theta_CB, d):
+    def hole_angle(self, ballx, bally, theta_CB = None, d = None):
         hole_x, hole_y = self.holes[:,0], -self.holes[:,1]
         theta_BH = -numpy.arctan2(hole_y + bally, hole_x - ballx) + PI/2
         theta_BH = wrap_angle(theta_BH)
-        theta_e = theta_BH - theta_CB
-        theta_e = wrap_angle(theta_e)
-        theta_m = PI/2 - math.asin(self.ball_radius * 2/d)
-        eligible_indices = numpy.where(abs(theta_e) < theta_m)
-        l = numpy.sqrt(d**2 + 4*(self.ball_radius**2) - 4 * d * self.ball_radius * numpy.cos(theta_e))
-        theta_C = numpy.arcsin(2 * self.ball_radius * numpy.sin(theta_e) / l)
-        return theta_C, eligible_indices
-
+        return theta_BH
 
     def cue_dist(self, ballx, bally, cuex, cuey):
         dist = numpy.square(ballx - cuex) + numpy.square(bally - cuey)
         dist = numpy.sqrt(dist)
         return dist
     
-    def pot_ball(self, ballx, bally, cuex, cuey):
+    def pot_ball(self, ballx, bally, cue_angle, d):
+        hole_angles = self.hole_angle(ballx, bally)
+        theta_m = PI/2 - math.asin(self.ball_radius * 2/d)
+        valid_holes = []
+        angle_error = hole_angles - cue_angle
+        l = numpy.sqrt(d**2 + 4*(self.ball_radius**2) - 4 * d * self.ball_radius * numpy.cos(angle_error))
+        shot_angle = numpy.arcsin(2 * self.ball_radius * numpy.sin(angle_error) / l)
+        for i in range(6):
+            if abs(angle_error[i]) < theta_m :
+                valid_holes.append(i)
+        valid_shot_angles = []
+        for hole in range(6):
+            # if hole not in valid_holes :
+                # continue
+            valid_shot_angles.append(shot_angle[hole])
+            
+        ret_angle = 0
+        force = 0
+        if len(valid_shot_angles) > 0 :
+            ret_angle = -min(numpy.absolute(valid_shot_angles)) - cue_angle
+            # print((-min(numpy.absolute(valid_shot_angles)))*180/PI)
+            # ret_angle = -cue_angle -valid_shot_angles[self.closest_hole(ballx, bally, valid_holes)]
+            # force = 0.8*d/1000 + 0.1
+            force = 0.5
+        else :
+            # print(" GOING BLIND")
+            ret_angle = -cue_angle
+            force = 1
 
-
-
-    def get_shot_angles(self, ball_pos):
-        balls = list(ball_pos.keys())
-        balls.remove('white')
-        if 0 in balls :
-            balls.remove(0)
-        cue_x, cue_y = ball_pos['white']
-        balls.sort()
-        x_coords = [ball_pos[i][0] for i in balls]
-        y_coords = [ball_pos[i][1] for i in balls]
-        dist = self.cue_dist(x_coords, y_coords, cue_x, cue_y)
-        shot_dict = {}
-        theta_CB = -numpy.arctan2(cue_y - y_coords, x_coords - cue_x) + PI/2 
-        theta_CB = wrap_angle(theta_CB)
-        for i, ball in enumerate(balls): # Might have to do balls enum because ball index and ball is not same            
-            shot_angles, indices = self.hole_angle(x_coords[i], y_coords[i], theta_CB[i], dist[i])
-            shot_dict[ball] = shot_angles
-        self.prev_balls = len(balls)
-        return shot_dict, theta_CB
+        # print(valid_holes)
+        return [ret_angle, force]
+    
+    def choose_ball(self, actions, index = 1):
+        min_key = None
+        min_value = float('inf')
+        for key, arr in actions.items():
+            if arr[index] < min_value:
+                min_key = key
+                min_value = arr[index]
+        return min_key
 
 
     def action(self, ball_pos=None):
@@ -86,18 +100,19 @@ class Agent:
         balls.remove('white')
         if 0 in balls :
             balls.remove(0)
+        # balls.sort()
         cue_x, cue_y = ball_pos['white']
-        balls.sort()
         x_coords = [ball_pos[i][0] for i in balls]
         y_coords = [ball_pos[i][1] for i in balls]
-
-
-
-
-        
-        shot_dict, CB_angle = self.get_shot_angles(ball_pos)
-        target = list(shot_dict.keys())[0]
-        shot_angle = shot_dict[target][0] + CB_angle[0]
-        force = 0.5
-        # print(shot_dict)
+        dist = self.cue_dist(x_coords, y_coords, cue_x, cue_y)
+        theta_CB = -numpy.arctan2(cue_y - y_coords, x_coords - cue_x) + PI/2 
+        theta_CB = wrap_angle(theta_CB)
+        actions_for_balls = {}
+        for i, ball in enumerate(balls) :
+            actions_for_balls[i] = self.pot_ball(x_coords[i], y_coords[i], theta_CB[i], dist[i]) # [angle, force]
+        chosen_ball = self.choose_ball(actions_for_balls, 1)
+        # chosen_ball = 0
+        print(actions_for_balls)
+        shot_angle = actions_for_balls[chosen_ball][0]
+        force = actions_for_balls[chosen_ball][1]
         return (shot_angle/PI ,force)
